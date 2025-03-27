4ac7b23b7d31f33d00c0b6d3267c6563970733a8
Melissa Lober <47082241+mlober@users.noreply.github.com>
2025-03-27 10:18:03 +0100

    Update simulation_3.py

--- multi-area-model/multiarea_model/simulation_3.py
+++ multi-area-model/multiarea_model/simulation_3.py
@@ -160,10 +160,7 @@ class Simulation:
                               'overwrite_files': True,
                               'data_path': os.path.join(self.data_dir, 'recordings'),
                               'print_time': False,
-                              'rng_seed': rng_seed,
-                              'spike_buffer_grow_extra': self.params['spike_buffer_grow_extra'],
-                              'spike_buffer_shrink_limit': self.params['spike_buffer_shrink_limit'],
-                              'spike_buffer_shrink_spare': self.params['spike_buffer_shrink_spare']})
+                              'rng_seed': rng_seed})
 
         # nest.set_verbosity('M_INFO')
 
