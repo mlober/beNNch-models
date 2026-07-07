59a95be4567c3a32631668243f4f275758687d0d
Melissa Lober <47082241+mlober@users.noreply.github.com>
2026-07-07 18:40:26 +0200

    Update simulation_3.py

--- multi-area-model/multiarea_model/simulation_3.py
+++ multi-area-model/multiarea_model/simulation_3.py
@@ -449,10 +449,12 @@ class Simulation:
                                                'cycle_time_log',
                                                str(nest.Rank()))))
 
-        np.savetxt(fn_cycle_time, np.transpose([d['cycle_time_log']['times'], d['cycle_time_log']['communicate_time'],
-                                                d['cycle_time_log']['communicate_time_global'], d['cycle_time_log']['communicate_time_local'],
-                                                d['cycle_time_log']['synch_time'], d['cycle_time_log']['local_spike_counter']]))
+        #np.savetxt(fn_cycle_time, np.transpose([d['cycle_time_log']['times'], d['cycle_time_log']['communicate_time'],
+        #                                        d['cycle_time_log']['communicate_time_global'], d['cycle_time_log']['communicate_time_local'],
+        #                                        d['cycle_time_log']['synch_time'], d['cycle_time_log']['local_spike_counter']]))
 
+        np.savetxt(fn_cycle_time, np.transpose([d['cycle_time_log']['times'], d['cycle_time_log']['communicate_time'], 
+                                                d['cycle_time_log']['local_spike_counter']]))
 
     def save_network_gids(self):
         with open(os.path.join(self.data_dir,
